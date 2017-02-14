a015a408826dd27e11ea693952e335617fe46ad8
alvaro <Alvarojosepl@gmail.com>
2017-02-14 14:16:47 -0600

    validate maximum consecutive numbers

--- test/converters/roman-to-arabic-test.js
+++ test/converters/roman-to-arabic-test.js
@@ -19,10 +19,57 @@ test('Convert direct Numbers', function (t) {
         t.equal(romanToArabic(numeral),map[numeral])
     }
     t.end()
-});
+})
 
 test('Get valid Subgroups', function (t) {
     t.deepEqual(romanToArabic._.getSubgroups('X','XXXIX'),['XXX', 'IX'])
     t.end()
-});
+})
+
+test('Valid Subgroups maximum repeat of 3 valid Group', function (t) {
+    var group = ['I','X','C','M']
+    for (var value of group){
+        var valid = Array(4).join(value)
+        t.equal(romanToArabic._.validateGroup(value, valid), true)
+    }
+    t.end()
+})
+
+test('Invalid Subgroups maximum repeat of 3 valid Group', function (t) {
+    var group = ['I','X','C','M']
+    for (let value of group){
+        let invalid = Array(5).join(value)
+        t.equal(romanToArabic._.validateGroup(value, invalid), false)
+    }
+    t.end()
+})
+
+
+test('Valid Subgroups maximum repeat of single group', function (t) {
+    var group = ['D','L','V']
+    for (var value of group){
+        t.equal(romanToArabic._.validateGroup(value, value), true)
+    }
+    t.end()
+})
+
+test('Invalid Subgroups maximum repeat of single group', function (t) {
+    var group = ['D','L','V']
+    for (let value of group){
+        let invalid = Array(3).join(value)
+        console.log(invalid)
+        t.equal(romanToArabic._.validateGroup(value, invalid), false)
+    }
+    t.end()
+})
+
+// test('Invalid Subgroups maximum repeat passed', function (t) {
+//     t.equal(romanToArabic._.validateGroup('X','XXXX'), false)
+//     t.end()
+// })
+
+// test('Get invalid Subgroups', function (t) {
+//     t.throws(romanToArabic._.getSubgroups('X','XXXXIX'))
+//     t.end()
+// })
 
