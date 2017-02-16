c788b2893a91acb25f32b1f42a2218b53cb95092
alvaro <Alvarojosepl@gmail.com>
2017-02-15 18:43:15 -0600

    added the data mapping to the export

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ romanToArabicConverter = require('./lib/conversion/roman-to-arabic')
 
 module.exports.ArabicToRoman = require('./lib/conversion/arabic-to-roman').convert
 module.exports.RomanToArabic = require('./lib/conversion/roman-to-arabic').convert
+module.exports.dataMapping = require('./lib/conversion/convertion-map')
